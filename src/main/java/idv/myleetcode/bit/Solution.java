cecee57fcd5b79564ce3233531c7d7f1bbed5995
Dixentw <dixentw@gmail.com>
2016-08-23 14:17:51 +0800

    add bit manipulation

--- src/main/java/idv/myleetcode/bit/Solution.java
+++ src/main/java/idv/myleetcode/bit/Solution.java
@@ -28,4 +28,49 @@ public class Solution{
         }
         return result;
     }
+    //371. sum with bits
+    public int getSum(int a, int b) {
+        int result = 0;
+        int carryBit = 0;
+        for(int i=0; i<32; i++){
+            int aBit = (a >> i) & 1 ;
+            int bBit = (b >> i) & 1 ;
+            result = result | ( (aBit^bBit^carryBit) << i );
+            carryBit = (carryBit&aBit) | (carryBit&bBit) | (aBit&bBit);
+        }
+        return result;
+    }
+    // 338. Counting Bits
+    private int countB(int num){
+        if(num<=0){
+            return 0;
+        }
+        if(num%2==1){
+            return 1+ countB(num/2);
+        }else{
+            return countB(num/2);
+        }
+    }
+    public int[] countBits(int num) {
+        int[] result = new int[num+1];
+        for(int i=0; i<=num; i++){
+            result[i] = countB(i);
+        }
+        return result;
+    }
+    public int[] countBits2(int num) {
+        int[] result = new int[num+1];
+        result[0] = 0;
+        result[1] = 1;
+        int pow = 2;
+        for(int i=2, t=0; i<=num; i++, t++){
+            if(i==pow){
+                pow = pow*2;
+                t=0;
+            }
+            result[i] = result[t]+1;
+        }
+        return result;
+    }
+
 }
--- src/test/java/idv/myleetcode/bit/SolutionTest.java
+++ src/test/java/idv/myleetcode/bit/SolutionTest.java
@@ -19,4 +19,15 @@ public class SolutionTest {
     public void test2(){
         assertEquals(1073741824, s.reverseBit(2));
     }
+    @Test
+    public void test3(){
+        assertEquals(7, s.getSum(2, 5));
+        assertEquals(4, s.getSum(1, 3));
+    }
+    @Test
+    public void test4(){
+        int[] result = s.countBits2(15);
+        int[] expect = new int[]{0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
+        assertTrue(Arrays.equals(expect, result));
+    }
 }
