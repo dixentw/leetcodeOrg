929004c63cc08a51686b79aa4b33058332d69da3
Dixen Cheng <dixen@17.media>
2017-04-17 21:05:37 +0800

    add solution

--- src/main/java/idv/myleetcode/array/Solution.java
+++ src/main/java/idv/myleetcode/array/Solution.java
@@ -1067,7 +1067,7 @@ public class Solution{
 	public boolean searchMatrix(int[][] matrix, int target) {
 		int rows = matrix.length;
 		if(rows==0){
-		    return false;
+			return false;
 		}
 		int cols = matrix[0].length;
 		int start = 0;
@@ -1100,13 +1100,13 @@ public class Solution{
 				nums[b++] = 2;
 			}
 		}
-    }
+	}
 	//78. Subsets
 	public List<List<Integer>> subsets(int[] nums) {
 		List<List<Integer>> result = new ArrayList<>();
 		subHelper(result, nums, new ArrayList<Integer>(), 0);
 		return result;
-    }
+	}
 	private void subHelper(List<List<Integer>> result, int[] nums, List<Integer> tmp, int start){
 		result.add(new ArrayList<Integer>(tmp));
 		for(int i=start; i<nums.length;i++){
@@ -1197,4 +1197,55 @@ public class Solution{
 		}
 		return pIdx++;
 	}
+	//81. Search in Rotated Sorted Array II
+	public boolean search_d(int[] nums, int target) {
+		int start = 0;
+		int end = nums.length -1;
+		while(start<=end){
+			int mid = (start+end) / 2;
+			if(nums[mid]==target){
+				return true;
+			}else if(target > nums[mid]){
+				if(nums[start]>nums[mid]&&target>=nums[start]){
+					end = mid - 1;
+				}else{
+					start = mid+1;
+				}
+			}else{
+				if(nums[mid]>=nums[end]&&target<=nums[end]){
+					start = mid+1;
+				}else{
+					end = mid - 1;
+				}
+			}
+		}
+		return false;
+    }
+	public boolean search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        //binary search
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(nums[mid] == target){
+                return true;
+            }else if(nums[l] <= nums[mid]){
+                //target is in the left part of situation 2
+                if(nums[l] <= target && target <= nums[mid]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                //target is in the right part of situation 3
+                if(nums[mid] <= target && target <= nums[r]){
+                    l = mid + 1;
+                }else{
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
 }
