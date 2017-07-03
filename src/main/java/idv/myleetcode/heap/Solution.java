01845de6764ee0fffbf0fcedf92c56ef429fe1da
Dixen Cheng <dixen@17.media>
2017-07-03 14:51:05 +0800

    update solution

--- src/main/java/idv/myleetcode/heap/Solution.java
+++ src/main/java/idv/myleetcode/heap/Solution.java
@@ -37,5 +37,56 @@ public class Solution{
             maxHeapify(A, len--, 0);
         }
     }
+	// min heap
+	public void minHeapify(int[] A, int arrLen, int i){
+		int leftChild = i*2+1;
+		int rightChild = i*2+2;
+		int smallest = i;
+		if(leftChild<arrLen&&A[leftChild]<A[smallest]){
+			smallest = leftChild;
+		}
+		if(rightChild<arrLen&&A[rightChild]<A[smallest]){
+			smallest = rightChild;
+		}
+		if(smallest != i){
+			int tmp = A[i];
+			A[i]  = A[smallest];
+			A[smallest] = tmp;
+			minHeapify(A, arrLen, smallest);
+		}
+	}
+	public void buildMinHeap(int[] A) {
+		int lastIdx = A.length-1;
+		for(int i=lastIdx/2; i>=0; i--){
+			minHeapify(A, A.length, i);
+		}
+	}
+	public void heapSortMin(int[] A){
+		buildMinHeap(A);
+		for(int i=A.length-1; i>=0; i--){
+			int root = A[0];
+            A[0] = A[i];
+            A[i] = root;
+            minHeapify(A, i, 0);
+		}
+		System.out.println(Arrays.toString(A));
+	}
+	public int[] smallestRange(int[][] A ){
+		int[] heap = new int[A.length];
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int range = Integer.MAX_VALUE;
+
+		for(int i=0; i<A.length; i++){
+			heap[i] = A[i][0];
+			max = Math.max(max, heap[i]);
+		}
+		buildMinHeap(heap);
+		while(1){
+			min = heap[0];
+			range = Math.min(range, max-min+1);
+
+		}
+	}
 
 }
--- src/test/java/idv/myleetcode/heap/SolutionTest.java
+++ src/test/java/idv/myleetcode/heap/SolutionTest.java
@@ -13,4 +13,17 @@ public class SolutionTest{
         int[] expect = new int[]{1,2,9,12,23,30,50};
         assertTrue(Arrays.equals(expect, unsort));
     }
+	@Test
+    public void test2(){
+        int[] unsort = new int[]{1, 23, 12, 9, 30, 2, 50};
+        s.heapSortMin(unsort);
+        int[] expect = new int[]{50,30,23,12,9,2,1};
+        assertTrue(Arrays.equals(expect, unsort));
+		int[][] a = new int[]{
+			{1, 3, 5, 7, 9},
+			{0, 2, 4, 6, 8},
+			{2, 3, 5, 7, 11}
+		}
+		s.smallestRange(a);
+    }
 }
