73dadd2040f7eb695786324d5083e5ee25b228fe
Dixen Cheng <dixen@17.media>
2017-10-10 22:41:37 +0800

    update solution

--- src/main/java/idv/myleetcode/string/Solution.java
+++ src/main/java/idv/myleetcode/string/Solution.java
@@ -3,197 +3,316 @@ package idv.myleetcode.string;
 import java.util.*;
 
 public class Solution{
-	//14. Longest Common Prefix
-	public String longestCommonPrefix(String[] strs) {
-		if(strs.length==0){
-			return "";
-		}
-		String pre = strs[0];
-		for(int i=1; i<strs.length; i++){
-			while(strs[i].indexOf(pre)!=0){
-				pre = pre.substring(0, pre.length()-1);
-			}
-		}
-		return pre;
+    //14. Longest Common Prefix
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0){
+            return "";
+        }
+        String pre = strs[0];
+        for(int i=1; i<strs.length; i++){
+            while(strs[i].indexOf(pre)!=0){
+                pre = pre.substring(0, pre.length()-1);
+            }
+        }
+        return pre;
     }
-	//28. Implement strStr()
-	public int strStr_s(String haystack, String needle) {
-		for(int i=0; i<haystack.length();i++){
-			boolean isDone = true;
-			for(int j=0; j<needle.length();j++){
-				if(i+j==haystack.length()){
-					return -1;
-				}
-				if(haystack.charAt(i+j)!=needle.charAt(j)){
-					isDone = false;
-					break;
-				}
-			}
-			if(isDone){
-				return i;
-			}
-		}
-		return -1;
+    //28. Implement strStr()
+    public int strStr_s(String haystack, String needle) {
+        for(int i=0; i<haystack.length();i++){
+            boolean isDone = true;
+            for(int j=0; j<needle.length();j++){
+                if(i+j==haystack.length()){
+                    return -1;
+                }
+                if(haystack.charAt(i+j)!=needle.charAt(j)){
+                    isDone = false;
+                    break;
+                }
+            }
+            if(isDone){
+                return i;
+            }
+        }
+        return -1;
     }
-	public int strStr(String haystack, String needle){
-		for (int i = 0; ; i++) {
-			for (int j = 0; ; j++) {
-				if (j == needle.length()) return i;
-				if (i + j == haystack.length()) return -1;
-				if (needle.charAt(j) != haystack.charAt(i + j)) break;
-			}
-		}
-	}
-	//38. Count and Say
-	public String countAndSay(int n) {
-		String seed = "1";
-		for(int i=1; i<n; i++){
-			String tmp = "";
-			for(int j=0; j<seed.length(); j++){
-				int count = 1;
-				while(j+1<seed.length()&&seed.charAt(j)==seed.charAt(j+1)){
-					count++;
-					j++;
-				}
-				tmp += String.valueOf(count)+seed.charAt(j);
-			}
-			seed = tmp;
-		}
-		return seed;
+    public int strStr(String haystack, String needle){
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
     }
-	//58. Length of Last Word
-	public int lengthOfLastWord(String s) {
-		s = s.trim();
-		String[] words = s.split(" ");
-		if(words.length==0){
-			return 0;
-		}
-		return words[words.length-1].length();
+    //38. Count and Say
+    public String countAndSay(int n) {
+        String seed = "1";
+        for(int i=1; i<n; i++){
+            String tmp = "";
+            for(int j=0; j<seed.length(); j++){
+                int count = 1;
+                while(j+1<seed.length()&&seed.charAt(j)==seed.charAt(j+1)){
+                    count++;
+                    j++;
+                }
+                tmp += String.valueOf(count)+seed.charAt(j);
+            }
+            seed = tmp;
+        }
+        return seed;
     }
-	//345. Reverse Vowels of a String
-	public String reverseVowels(String s) {
-		String vowels = "aeiouAEIOU";
-		char[] cs = s.toCharArray();
-		int start = 0;
-		int end = cs.length-1;
-		while(start<end){
-			if(vowels.indexOf(cs[start])==-1){//not vowel
-				start++;
-			}
-			if(vowels.indexOf(cs[end])==-1){
-				end--;
-			}
-			if(start<end&&vowels.indexOf(cs[start])!=-1&&vowels.indexOf(cs[end])!=-1){
-				char tmp = cs[start];
-				cs[start++] = cs[end];
-				cs[end--] = tmp;
-			}
-		}
-		return new String(cs);
+    //58. Length of Last Word
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] words = s.split(" ");
+        if(words.length==0){
+            return 0;
+        }
+        return words[words.length-1].length();
     }
-	//434. Number of Segments in a String
-	public int countSegments(String s) {
-		s = s.trim();
-		String[] data = s.split("\\s+");
-		return data.length;
+    //345. Reverse Vowels of a String
+    public String reverseVowels(String s) {
+        String vowels = "aeiouAEIOU";
+        char[] cs = s.toCharArray();
+        int start = 0;
+        int end = cs.length-1;
+        while(start<end){
+            if(vowels.indexOf(cs[start])==-1){//not vowel
+                start++;
+            }
+            if(vowels.indexOf(cs[end])==-1){
+                end--;
+            }
+            if(start<end&&vowels.indexOf(cs[start])!=-1&&vowels.indexOf(cs[end])!=-1){
+                char tmp = cs[start];
+                cs[start++] = cs[end];
+                cs[end--] = tmp;
+            }
+        }
+        return new String(cs);
     }
-	//383. Ransom Note
-	public boolean canConstruct(String ransomNote, String magazine) {
-		int[] table = new int[128];
-		for(char c : magazine.toCharArray()){
-			table[c]++;
-		}
-		for(char c: ransomNote.toCharArray()){
-			table[c]--;
-			if(table[c] < 0){
-				return false;
-			}
-		}
-		return true;
+    //434. Number of Segments in a String
+    public int countSegments(String s) {
+        s = s.trim();
+        String[] data = s.split("\\s+");
+        return data.length;
     }
-	//5. Longest Palindromic Substring
-	private int low = 0;
-	private int maxLen = 0;
-	public String longestPalindrome(String s) {
-		if(s.length()<2){
-			return s;
-		}
-		for(int i=0; i<s.length()-1; i++){
-			extendPalindrome(s, i, i);
-			extendPalindrome(s, i, i+1);
-		}
-		return s.substring(low, low+maxLen);
+    //383. Ransom Note
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] table = new int[128];
+        for(char c : magazine.toCharArray()){
+            table[c]++;
+        }
+        for(char c: ransomNote.toCharArray()){
+            table[c]--;
+            if(table[c] < 0){
+                return false;
+            }
+        }
+        return true;
     }
-	private void extendPalindrome(String s, int start, int end){
-		while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
-			start--;
-			end++;
-		}
-		// the len should be end-start+1, but we need perform end--, start++ again, so it become end-start-1
-		if(end-start-1> maxLen){
-			low = start+1;//not include start, since now the start break the palindrome
-			maxLen = end-start-1;
-		}
-	}
-	//6. ZigZag Conversion
-	public String convert(String s, int numRows) {
-		StringBuffer[] sbs = new StringBuffer[numRows];
-		for(int k=0; k<numRows; k++){
-			sbs[k] = new StringBuffer();
-		}
-		int i = 0;
-		while(i<s.length()){
-			for(int idx=0; idx<numRows&&i<s.length(); idx++){
-				sbs[idx].append(s.charAt(i++));
-			}
-			for(int idx=numRows-2; idx>=1&&i<s.length(); idx--){
-				sbs[idx].append(s.charAt(i++));
-			}
-		}
-		String result = "";
-		for(i=0; i<sbs.length; i++){
-			result += sbs[i].toString();
-		}
-		return result;
+    //5. Longest Palindromic Substring
+    private int low = 0;
+    private int maxLen = 0;
+    public String longestPalindrome(String s) {
+        if(s.length()<2){
+            return s;
+        }
+        for(int i=0; i<s.length()-1; i++){
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i+1);
+        }
+        return s.substring(low, low+maxLen);
     }
-	//17. Letter Combinations of a Phone Number
-	public List<String> letterCombinations(String digits) {
-		String[] keyMap = {"","","abc", "def", "ghi","jkl","mno", "pqrs", "tuv", "wxyz"};
-		List<String> result = new ArrayList<>();
-		if(digits.length()==0){
-			return result;
-		}
-		comb(result, "", digits, 0, keyMap);
-		return result;
-	}
-	private void comb(List<String> result, String prefix, String digits, int offset, String[] keyMap){
-		if(offset==digits.length()){
-			result.add(prefix);
-			return;
-		}
-		String letters = keyMap[digits.charAt(offset)-'0'];
-		for(int i=0; i<letters.length(); i++){
-			comb(result, prefix+letters.charAt(i), digits, offset+1, keyMap);
-		}
-	}
-	//22. Generate Parentheses
-	public List<String> generateParenthesis(int n) {
-		List<String> result = new ArrayList<>();
-		genP(result, "", n, n);
-		return result;
+    private void extendPalindrome(String s, int start, int end){
+        while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
+            start--;
+            end++;
+        }
+        // the len should be end-start+1, but we need perform end--, start++ again, so it become end-start-1
+        if(end-start-1> maxLen){
+            low = start+1;//not include start, since now the start break the palindrome
+            maxLen = end-start-1;
+        }
     }
-	private void genP(List<String> res, String tmp, int left, int right){
-		if(left==0&&right==0){
-			res.add(tmp);
-		}
-		if(left>right){
-			return;
-		}
-		if(left>0){
-			genP(res, tmp+"(", left-1, right);
-		}
-		if(right>0){
-			genP(res, tmp+")", left, right-1);
-		}
-	}
+    //6. ZigZag Conversion
+    public String convert(String s, int numRows) {
+        StringBuffer[] sbs = new StringBuffer[numRows];
+        for(int k=0; k<numRows; k++){
+            sbs[k] = new StringBuffer();
+        }
+        int i = 0;
+        while(i<s.length()){
+            for(int idx=0; idx<numRows&&i<s.length(); idx++){
+                sbs[idx].append(s.charAt(i++));
+            }
+            for(int idx=numRows-2; idx>=1&&i<s.length(); idx--){
+                sbs[idx].append(s.charAt(i++));
+            }
+        }
+        String result = "";
+        for(i=0; i<sbs.length; i++){
+            result += sbs[i].toString();
+        }
+        return result;
+    }
+    //17. Letter Combinations of a Phone Number
+    public List<String> letterCombinations(String digits) {
+        String[] keyMap = {"","","abc", "def", "ghi","jkl","mno", "pqrs", "tuv", "wxyz"};
+        List<String> result = new ArrayList<>();
+        if(digits.length()==0){
+            return result;
+        }
+        comb(result, "", digits, 0, keyMap);
+        return result;
+    }
+    private void comb(List<String> result, String prefix, String digits, int offset, String[] keyMap){
+        if(offset==digits.length()){
+            result.add(prefix);
+            return;
+        }
+        String letters = keyMap[digits.charAt(offset)-'0'];
+        for(int i=0; i<letters.length(); i++){
+            comb(result, prefix+letters.charAt(i), digits, offset+1, keyMap);
+        }
+    }
+    //22. Generate Parentheses
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        genP(result, "", n, n);
+        return result;
+    }
+    private void genP(List<String> res, String tmp, int left, int right){
+        if(left==0&&right==0){
+            res.add(tmp);
+        }
+        if(left>right){
+            return;
+        }
+        if(left>0){
+            genP(res, tmp+"(", left-1, right);
+        }
+        if(right>0){
+            genP(res, tmp+")", left, right-1);
+        }
+    }
+    // 91. Decode Ways
+    public int numDecodings(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) - '0' != 0 ? 1 : 0;
+        for(int i=2; i<=n; i++){
+            int first = Integer.valueOf(s.substring(i-1, i));
+            int second = Integer.valueOf(s.substring(i-2, i));
+            if(first != 0){
+                dp[i] += dp[i-1];
+            }
+            if(second>=10 && second <=26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+    // 43. Multiply Strings
+    public String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
+        int[] buf = new int[m+n];
+        for(int i=n-1;i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int cur = (num1.charAt(j)-'0') * (num2.charAt(i)-'0');
+                int p1 = i+j;
+                int p2 = i+j+1;
+                int sum = cur + buf[p2];
+                buf[p1] += sum/10;
+                buf[p2] = sum%10;
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for(int p : buf){
+            if(sb.length()==0 && p==0){
+                continue;
+            }
+            sb.append(p);
+        }
+        if(sb.length()==0){
+            return "0";
+        }
+        return sb.toString();
+    }
+    // 93. Restore IP Addresses
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        restoreIP(res, s, "", 0);
+        return res;
+    }
+    private void restoreIP(List<String> res, String s, String tmp, int index){
+        int dotCount = 0;
+        for(int i=0; i<tmp.length(); i++){
+            if(tmp.charAt(i)=='.'){
+                dotCount++;
+            }
+        }
+        if(dotCount>=3){
+            if(dotCount==3&&index==s.length()){
+                res.add(tmp);
+            }
+            return;
+        }
+        for(int i=1; i<4; i++){
+            if(index+i > s.length()){
+                continue;
+            }
+            String part = s.substring(index, index+i);
+            if(Integer.parseInt(part) > 255){
+                continue;
+            }
+            if(part.startsWith("0")&&part.length()>1){
+                continue;
+            }
+            String orig = tmp;
+            if(tmp.length()==0){
+                tmp = part;
+            }else{
+                tmp = tmp+"."+part;
+            }
+            restoreIP(res, s, tmp, index+i);
+            tmp = orig;
+        }
+    }
+    // 165. Compare Version Numbers
+    public int compareVersion(String version1, String version2) {
+        String[] v1Parts = version1.split("\\.");
+        String[] v2Parts = version2.split("\\.");
+        int shortLen = Math.min(v1Parts.length, v2Parts.length);
+        for(int i=0; i<shortLen; i++){
+            int v1 = Integer.parseInt(v1Parts[i]);
+            int v2 = Integer.parseInt(v2Parts[i]);
+            if(v1>v2){
+                return 1;
+            }
+            if(v1<v2){
+                return -1;
+            }
+        }
+        if(v1Parts.length>v2Parts.length){
+            for(int i=shortLen; i<v1Parts.length; i++){
+                if(!v1Parts[i].matches("0+")){
+                    return 1;
+                }
+            }
+        }
+        if(v1Parts.length<v2Parts.length){
+            for(int i=shortLen; i<v2Parts.length; i++){
+                if(!v2Parts[i].matches("0+")){
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
 }
--- src/test/java/idv/myleetcode/string/SolutionTest.java
+++ src/test/java/idv/myleetcode/string/SolutionTest.java
@@ -8,72 +8,96 @@ public class SolutionTest{
     Solution s = new Solution();
     @Test
     public void test14(){
-		String[] strs = new String[]{
-			"abc23423423",
-			"abc43423423",
-			"abc63423423",
-			"abc83423423",
-		};
-		String res = s.longestCommonPrefix(strs);
-		assertEquals("abc", res);
+        String[] strs = new String[]{
+            "abc23423423",
+            "abc43423423",
+            "abc63423423",
+            "abc83423423",
+        };
+        String res = s.longestCommonPrefix(strs);
+        assertEquals("abc", res);
     }
-	@Test
-	public void test28(){
-		assertEquals(2, s.strStr("abcdef", "cd"));
-		assertEquals(-1, s.strStr("abcdef", "xyz"));
-		assertEquals(-1, s.strStr("mississippi", "issipi"));
+    @Test
+    public void test28(){
+        assertEquals(2, s.strStr("abcdef", "cd"));
+        assertEquals(-1, s.strStr("abcdef", "xyz"));
+        assertEquals(-1, s.strStr("mississippi", "issipi"));
 
-	}
-	@Test
-	public void test38(){
-		assertEquals("1", s.countAndSay(1));
-		assertEquals("11", s.countAndSay(2));
-		assertEquals("21", s.countAndSay(3));
-		assertEquals("1211", s.countAndSay(4));
-		assertEquals("111221", s.countAndSay(5));
-		assertEquals("312211", s.countAndSay(6));
-	}
-	@Test
-	public void test58(){
-		assertEquals(5, s.lengthOfLastWord("hello world"));
-	}
+    }
+    @Test
+    public void test38(){
+        assertEquals("1", s.countAndSay(1));
+        assertEquals("11", s.countAndSay(2));
+        assertEquals("21", s.countAndSay(3));
+        assertEquals("1211", s.countAndSay(4));
+        assertEquals("111221", s.countAndSay(5));
+        assertEquals("312211", s.countAndSay(6));
+    }
+    @Test
+    public void test58(){
+        assertEquals(5, s.lengthOfLastWord("hello world"));
+    }
 
-	@Test
-	public void test345(){
-		assertEquals("leotcede", s.reverseVowels("leetcode"));
-		assertEquals("aA", s.reverseVowels("Aa"));
-		assertEquals("a.b,.", s.reverseVowels("a.b,."));
-	}
-	@Test
-	public void test434(){
-		assertEquals(5, s.countSegments("Hello, my name is John"));
-		assertEquals(6, s.countSegments(", , , ,        a, eaefa"));
-	}
-	@Test
-	public void test383(){
-		assertFalse(s.canConstruct("a", "b"));
-		assertTrue(s.canConstruct("aa", "aab"));
-		assertFalse(s.canConstruct("aa", "ab"));
-	}
-	@Test
-	public void test5(){
-		assertEquals("bab", s.longestPalindrome("babad"));
-	}
-	@Test
-	public void test6(){
-		assertEquals("PAHNAPLSIIGYIR", s.convert("PAYPALISHIRING", 3));
-	}
-	@Test
-	public void test17(){
-		List<String> res = s.letterCombinations("2");
-		assertEquals("[a, b, c]", Arrays.toString(res.toArray()));
-		res = s.letterCombinations("");
-		assertEquals("[]", Arrays.toString(res.toArray()));
-	}
-	@Test
-	public void test22(){
-		List<String> res = s.generateParenthesis(4);
-		System.out.println(Arrays.toString(res.toArray()));
-	}
-
+    @Test
+    public void test345(){
+        assertEquals("leotcede", s.reverseVowels("leetcode"));
+        assertEquals("aA", s.reverseVowels("Aa"));
+        assertEquals("a.b,.", s.reverseVowels("a.b,."));
+    }
+    @Test
+    public void test434(){
+        assertEquals(5, s.countSegments("Hello, my name is John"));
+        assertEquals(6, s.countSegments(", , , ,        a, eaefa"));
+    }
+    @Test
+    public void test383(){
+        assertFalse(s.canConstruct("a", "b"));
+        assertTrue(s.canConstruct("aa", "aab"));
+        assertFalse(s.canConstruct("aa", "ab"));
+    }
+    @Test
+    public void test5(){
+        assertEquals("bab", s.longestPalindrome("babad"));
+    }
+    @Test
+    public void test6(){
+        assertEquals("PAHNAPLSIIGYIR", s.convert("PAYPALISHIRING", 3));
+    }
+    @Test
+    public void test17(){
+        List<String> res = s.letterCombinations("2");
+        assertEquals("[a, b, c]", Arrays.toString(res.toArray()));
+        res = s.letterCombinations("");
+        assertEquals("[]", Arrays.toString(res.toArray()));
+    }
+    @Test
+    public void test22(){
+        List<String> res = s.generateParenthesis(4);
+        System.out.println(Arrays.toString(res.toArray()));
+    }
+    @Test
+    public void test91(){
+        assertEquals(2, s.numDecodings("12"));
+        assertEquals(1, s.numDecodings("10"));
+        assertEquals(1, s.numDecodings("27"));
+    }
+    @Test
+    public void test43(){
+        assertEquals("612", s.multiply("51", "12"));
+    }
+    @Test
+    public void test93(){
+        List<String> res = s.restoreIpAddresses("25525511135");
+        assertTrue(res.contains("255.255.11.135"));
+        assertTrue(res.contains("255.255.111.35"));
+    }
+    @Test
+    public void test165(){
+        assertEquals(-1, s.compareVersion("1.5", "2.5"));
+        assertEquals(-1, s.compareVersion("1", "1.1"));
+        assertEquals(0, s.compareVersion("1.0", "1"));
+        assertEquals(1, s.compareVersion("1.0.1", "1"));
+        assertEquals(0, s.compareVersion("2.5", "2.5"));
+        assertEquals(1, s.compareVersion("2.5.1", "2.5"));
+    }
 }
